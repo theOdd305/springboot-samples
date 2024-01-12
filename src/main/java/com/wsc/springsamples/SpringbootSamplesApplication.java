7c488a47e3b87e532c5129f38fae6beb67e9df0d
sicong <captainslow305@gmail.com>
2024-01-12 16:00:58 +0800

    新增数据库连接池和 mybatis 集成

--- src/main/java/com/wsc/springsamples/SpringbootSamplesApplication.java
+++ src/main/java/com/wsc/springsamples/SpringbootSamplesApplication.java
@@ -1,9 +1,11 @@
 package com.wsc.springsamples;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wsc.springsamples.mappers")
 public class SpringbootSamplesApplication {
 
     public static void main(String[] args) {
